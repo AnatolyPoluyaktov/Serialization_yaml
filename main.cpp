4b4ebc7bccad432684753ff06ae05345df2fdd8c
Anatoly Poluyaktov <avpoluyaktov@gmail.com>
2020-11-14 17:13:29 +0300

    check version libfyaml

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "yaml-cpp/yaml.h"
 #include <iostream>
+#include "libfyaml.h"
 int main()
 {
    YAML::Emitter out;
    out << "Hello, World!";
 
    std::cout << "Here's the output YAML:\n" << out.c_str(); // prints "Hello, World!"
+   printf("%s\n", fy_library_version());
    return 0;
 }
